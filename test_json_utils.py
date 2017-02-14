86f331715e92f40816a99fb5f6754f1be1dbaab1
MinSun <minsunp@andrew.cmu.edu>
2017-02-13 22:19:55 -0500

    Assignment2, Part1, Phase2

--- test_json_utils.py
+++ test_json_utils.py
@@ -1,5 +1,6 @@
-import test_data
 import sys
+
+import test_data
 import json
 
 #Creates and returns a GameLibrary object(defined in test_data) from loaded json_data
@@ -8,13 +9,35 @@ def make_game_library_from_json(json_data):
     game_library = test_data.GameLibrary()
 
     #Loop through the json_data
-        #Create a new Game object from the json_data by reading
+    for item in json_data: #item: dictionary
+        for key,value in item.items():
+
+            if key == "platform":
+                for k, v in value.items(): #value: dictionary
+                    if k == "launch year":
+                        launch_year = v
+                    else: #k == "name"
+                        name = v
+
+            elif key == "title":
+                title = value
+
+            else: #key == "year"
+                year = value
+
+        #For loop for item dictionary ends: put in test_data classes
+        # Create a new Game object from the json_data by reading
         #  title
         #  year
         #  platform (which requires reading name and launch_year)
-        #Add that Game object to the game_library
-    #Return the completed game_library
+        game_platform = test_data.Platform(name, launch_year)
+        game = test_data.Game(title, game_platform, year)
 
+        #Add that Game object to the game_library
+        game_library.add_game(game)
+
+    #json_data for loop ends.
+    #Return the completed game_library
     return game_library
 
 # Handling command line arguments
@@ -34,3 +57,9 @@ else:
 #Load the json data from the input file
 #Use make_game_library_from_json(json_data) to convert the data to GameLibrary data
 #Print out the resulting GameLibrary data using print_game_library(game_library_data) in test_data.py
+
+with open(input_json_file, 'r') as reader:
+    json_data = json.load(reader)
+
+game_library_data = make_game_library_from_json(json_data)
+test_data.print_game_library(game_library_data)
